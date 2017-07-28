bd235c0447fc4f395a409f87f582e33b2dc62151
Dominic Ginger <domginger@hotmail.co.uk>
2017-07-28 23:04:42 +0100

    update all comments ; -> ;;

--- exercise_1/exercises.rkt
+++ exercise_1/exercises.rkt
@@ -1,38 +1,38 @@
-; Exercise 1.1
-10 ; 10
-(+ 5 3 4) ; 12
-(- 9 1) ; 8
-(/ 6 2) ; 3
-(+ (* 2 4) (- 4 6)) ; 6
-(define a 3) ; 3
-(define b (+ a 1)) ; 4
-(+ a b (* a b)) ; 19
-(= a b) ; f
+;; Exercise 1.1
+10 ;; 10
+(+ 5 3 4) ;; 12
+(- 9 1) ;; 8
+(/ 6 2) ;; 3
+(+ (* 2 4) (- 4 6)) ;; 6
+(define a 3) ;; 3
+(define b (+ a 1)) ;; 4
+(+ a b (* a b)) ;; 19
+(= a b) ;; f
 (if (and (> b a) (< b (* a b)))
   b
-  a) ; 4
+  a) ;; 4
 (cond ((= a 4) 6)
       ((= b 4) (+ 6 7 a))
-      (else 25)) ; 16
-(+ 2 (if (> b a) b a)) ; 6
+      (else 25)) ;; 16
+(+ 2 (if (> b a) b a)) ;; 6
 (* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
-   (+ a 1)) ; 16
+   (+ a 1)) ;; 16
 
-; Exercise 1.2
+;; Exercise 1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
-   (* 3 (- 6 2) (- 2 7))) ; -37/150
+   (* 3 (- 6 2) (- 2 7))) ;; -37/150
 
-; Exercise 1.3
+;; Exercise 1.3
 (define (square x) (* x x))
 (define (sum-square-big x y z)
   (cond ((and (< x y) (< x z)) (+ (square y) (square z)))
         ((and (< y x) (< y z)) (+ (square x) (square z)))
         ((and (< z x) (< z y)) (+ (square x) (square y)))))
-(sum-square-big 19 23 21) ; 970
+(sum-square-big 19 23 21) ;; 970
 
-; Exercise 1.7
+;; Exercise 1.7
 (define (sqrt x)
   (define (average x y) (/ (+ x y) 2))
   (define (improve guess) (average guess (/ x guess)))
@@ -44,7 +44,7 @@
   (sqrt-iter 0.0 1.0))
 (sqrt 9)
 
-; Exercise 1.8
+;; Exercise 1.8
 (define (cubert x)
   (define (square a) (* a a))
   (define (improve guess)
@@ -58,8 +58,8 @@
   (cubert-iter 0.0 1.0))
 (cubert 27)
 
-; Exercise 1.11
-; recursive
+;; Exercise 1.11
+;; recursive
 (define (f n)
   (if (< n 3)
     n
@@ -68,7 +68,7 @@
        (* 3 (f (- n 3))))))
 (f 6)
 
-; iterative
+;; iterative
 (define (f n)
   (define (f-iter a b c count)
     (if (< count 3)
@@ -77,7 +77,7 @@
   (f-iter 0 1 2 n))
 (f 4)
 
-; Exercise 1.12
+;; Exercise 1.12
 (define (pascal x y)
   (if (or (= x 1) (= x y))
     1
@@ -85,7 +85,7 @@
        (pascal x (- y 1)))))
 (pascal 3 5)
 
-; Exercise 1.13
+;; Exercise 1.13
 (define (sqrt x)
   (define (average x y) (/ (+ x y) 2))
   (define (improve guess) (average guess (/ x guess)))
@@ -114,14 +114,14 @@
 (fib 10)
 (calc-fib 10)
 
-; Exercise 1.16
+;; Exercise 1.16
 (define (fast-expt b n)
   (cond ((= n 0) 1)
         ((even? n) (fast-expt (square b) (/ n 2)))
         (else (* b (fast-expt b (- n 1))))))
 (fast-expt 2 8)
 
-; Exercise 1.17
+;; Exercise 1.17
 (define (fast-multi x y)
   (define (double x) (+ x x))
   (define (halve x) (/ x 2))
@@ -131,7 +131,7 @@
 
 (fast-multi 31 300)
 
-; Exercise 1.18
+;; Exercise 1.18
 (define (fast-multi x y)
   (define (double x) (+ x x))
   (define (halve x) (/ x 2))
@@ -142,7 +142,7 @@
   (fast-multi-iter x y 0))
 (fast-multi 31 300)
 
-; Exercise 1.19
+;; Exercise 1.19
 (define (fib n)
   (fib-iter 1 0 0 1 n))
 (define (fib-iter a b p q count)
